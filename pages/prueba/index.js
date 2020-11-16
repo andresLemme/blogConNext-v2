a9bc95a91113f3bdc47ea4988325f8d26a3b7bfb
Pocas1982 <andres.lemme@gmail.com>
2020-11-16 12:22:12 -0300

    testing GetStaticProps

---
+++ pages/prueba/index.js
@@ -0,0 +1,22 @@
+
+export default function prueba({name, description}){
+  return(
+    <div>
+      <p>Hola, mi nombre en github es : {name} </p>
+      <p>{description}</p>
+    </div>
+  )
+}
+
+export async function getStaticProps(){
+
+  const data =  await fetch('https://api.github.com/users/andreslemme')
+  const json = await  data.json()
+
+  return{
+    props:{
+      name: json.name,
+      description: json.bio
+    }
+  }
+}
